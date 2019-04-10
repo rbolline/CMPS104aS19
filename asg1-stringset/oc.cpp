3e4d9df12ff81d32dda2190f9ae6d5aff6b6d232
Rohith Bollineni <rbolline@unix1.lt.ucsc.edu>
2019-04-09 19:33:31 -0700

    oc execbin, Makefile finished

--- asg1-stringset/oc.cpp
+++ asg1-stringset/oc.cpp
@@ -1,5 +1,35 @@
-#include "iostream"
+#include<iostream>
+#include <string>
+using namespace std;
+
+#include <errno.h>
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wait.h>
+#include <unistd.h>
 
 int main (int argc, char** argv) {
-	
+    int opt;
+    while((opt = getopt(argc, argv, "ly@D:")) != -1){
+        switch(opt) {
+            case 'l':
+                std::cout << 'l';
+                break;
+            case 'y':
+                std::cout << 'y';
+                break;
+            case '@':
+                std::cout << '@';
+                break;
+            case 'D':
+                std::cout << 'D';
+                break;
+            default:
+                std::cout << "No option";
+                break;
+
+        }
+    }
 }
