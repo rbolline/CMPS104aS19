0000a73d3af4eb53da4c1fc47f406bb92e6b1ba6
Ani Chaloyan <achaloya@unix2.lt.ucsc.edu>
2019-04-10 16:26:49 -0700

    added debugging

--- asg1-stringset/oc.cpp
+++ asg1-stringset/oc.cpp
@@ -4,24 +4,63 @@ using namespace std;
 
 #include <errno.h>
 #include <libgen.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <wait.h>
 #include <unistd.h>
+#include <cassert>
+
+const char* debugflags = "";
+bool alldebugflags = false;
+
+// Generate debugging code.
+void __debugprintf (char flag, const char* file, int line,
+                    const char* func, const char* format, ...);
+#define DEBUGF(FLAG,...) \
+        __debugprintf (FLAG, __FILE__, __LINE__, __PRETTY_FUNCTION__, \
+                       __VA_ARGS__)
+#define DEBUGSTMT(FLAG,STMTS) \
+       if (is_debugflag (FLAG)) { DEBUGF (FLAG, "\n"); STMTS }
+
+bool is_debugflag (char flag) {
+   return alldebugflags or strchr (debugflags, flag) != nullptr;
+}
+
+void __debugprintf (char flag, const char* file, int line,
+                    const char* func, const char* format, ...) {
+   va_list args;
+   if (not is_debugflag (flag)) return;
+   fflush (nullptr);
+   va_start (args, format);
+   fprintf (stderr, "DEBUGF(%c): %s[%d] %s():\n",
+             flag, file, line, func);
+   vfprintf (stderr, format, args);
+   va_end (args);
+   fflush (nullptr);
+}
+
+void set_debugflags (const char* flags) {
+	debugflags = flags;
+    assert (debugflags != nullptr);
+    if (strchr (debugflags, '@') != nullptr) alldebugflags = true;
+    DEBUGF ('x', "Debugflags = \"%s\", all = %d\n",
+           debugflags, alldebugflags);
+}
 
 int main (int argc, char** argv) {
     int opt;
-    while((opt = getopt(argc, argv, "ly@D:")) != -1){
+    while((opt = getopt(argc, argv, "ly@:D:")) != -1){
         switch(opt) {
             case 'l':
                 std::cout << 'l';
                 break;
             case 'y':
-                std::cout << 'y';
+      		    std::cout << 'y';
                 break;
             case '@':
-                std::cout << '@';
+                set_debugflags(optarg);
                 break;
             case 'D':
                 std::cout << 'D';
@@ -29,7 +68,8 @@ int main (int argc, char** argv) {
             default:
                 std::cout << "No option";
                 break;
-
         }
     }
 }
+
+
